564235ef38175b07e7ca3eec8cfdf94b554f12c9
Upgraded health <upgradedhealth1401@gmail.com>
2026-08-05 05:18:22 -0400

    Replace all files with pure-supps-deploy.zip for Hostinger upload

--- src/app/product/[slug]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from 'react-router-dom';
-import ProductPageClient from './ProductPageClient';
-
-export default function ProductPage() {
-  const { slug } = useParams<{ slug: string }>();
-  return <ProductPageClient slug={slug!} />;
-}
--- src/types/custom-elements.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    'split-lines': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-    'reveal-items': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-  }
-}
